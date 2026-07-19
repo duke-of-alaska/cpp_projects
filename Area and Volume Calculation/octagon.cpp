e6585c42cb0d23cee868711e5766225899439dae
duke-of-alaska <benedictus.marcello@gmail.com>
2026-07-19 22:27:52 +0700

    Add octagon area and volume calculator

---
+++ Area and Volume Calculation/octagon.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+double calculateArea(double length);
+double calculateVolume(double length, double height);
+
+int main(){
+    double length;
+
+    std::cout << "What is the length of the octagon in cm? ";
+    std::cin >> length;
+
+    double height;
+    std::cout << "What is the height of the octagon in cm? ";
+    std::cin >> height;
+
+    double result = calculateArea(length);
+    std::cout << result << "cm^2" << '\n';
+
+    double result2 = calculateVolume(length, height);
+    std::cout << result2 << "cm^3" << '\n';
+
+}
+
+double calculateArea(double length){
+    return 2 * (1 + std::sqrt(2)) * (length * length);
+}
+
+double calculateVolume(double length, double height){
+    return calculateArea(length) * height;
+}
