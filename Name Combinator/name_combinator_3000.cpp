ac8a56f0781590baa346d7d38729f83e1f060acd
duke-of-alaska <benedictus.marcello@gmail.com>
2026-07-21 15:33:30 +0700

    Removed unnecessary comments

--- Name Combinator/name_combinator_3000.cpp
+++ Name Combinator/name_combinator_3000.cpp
@@ -6,19 +6,19 @@ using str_t = std::string;
 str_t concatString(str_t firstName, str_t lastName);
 
 int main(){
-    str_t firstName; // First name
+    str_t firstName; 
 
     std::cout << "Enter your first name: ";
     std::getline(std::cin >> std::ws, firstName);
 
-    str_t lastName; // Last name
+    str_t lastName; 
 
     std::cout << "Enter your last name: ";
     std::getline(std::cin >> std::ws, lastName);
 
-    str_t combined_string = concatString(firstName, lastName); // Combine
+    str_t combined_string = concatString(firstName, lastName); 
 
-    std::cout << "Hello, " << combined_string << "!" << '\n'; // Output
+    std::cout << "Hello, " << combined_string << "!" << '\n';
     return 0;
 }
 
